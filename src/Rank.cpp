47cc4fc9e245dedefc1dc89a46d33b011a4e3cea
Avadanei Rares <avadaneirares0@gmail.com>
2025-05-05 23:34:12 +0300

    Create Rank.cpp

---
+++ src/Rank.cpp
@@ -0,0 +1,22 @@
+#include "Rank.h"
+#include <utility>
+
+Rank::Rank(std::string tier, int points) noexcept
+  : tier_{std::move(tier)}, points_{points}
+{}
+
+const std::string& Rank::tier() const noexcept {
+    return tier_;
+}
+
+int Rank::points() const noexcept {
+    return points_;
+}
+
+bool Rank::operator==(Rank const& o) const noexcept {
+    return tier_ == o.tier_ && points_ == o.points_;
+}
+
+bool Rank::operator!=(Rank const& o) const noexcept {
+    return !(*this == o);
+}
