83d550ba221e8bedd61f03d79bc741d7fff6d8b0
Avadanei Rares <avadaneirares0@gmail.com>
2025-05-05 23:43:24 +0300

    Create Server.h

---
+++ include/Server.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+#include "Player.h"
+#include "Rank.h"
+#include "Drafter.h"
+
+class Server {
+    std::string                name_;
+    Rank                       rank_;
+    std::vector<Player>        players_;
+    std::unique_ptr<Drafter>   drafter_;
+
+public:
+    Server(std::string name, Rank rank, std::unique_ptr<Drafter> dr);
+    Server(Server const& o);
+    Server& operator=(Server o);
+    ~Server() = default;
+
+    void addPlayer(Player p);
+    bool removePlayer(std::string const& name);
+    void setDrafter(std::unique_ptr<Drafter> dr);
+
+    Champion performDraft() const;
+    Champion getMostPopularChampion() const;
+    Champion getMedianChampion() const;
+
+    const std::vector<Player>& getPlayers() const noexcept;
+
+    friend void swap(Server& a, Server& b) noexcept;
+};
